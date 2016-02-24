0aea4170f28025002b02c97f1fcdd9cc3a3f5bd8
Tim Wilson <twilson@provision.org>
2016-02-24 15:05:06 -0800

    Updated the example app to use the latest version of the NuGet package and is now using some of the newer functionality of AutocompleteFor that I recently added.

---
+++ example/WebApplication2/packages/Twitter.Typeahead.MVC.Model.1.0.6/Content/Scripts/typeahead.mvc.model.js
@@ -0,0 +1,49 @@
+﻿$(document).ready($(function () {
+
+    function autocompletewrapper(obj) {
+        var autos = new Bloodhound({
+            datumTokenizer: function (datum) {
+                return Bloodhound.tokenizers.whitespace(datum.value);
+            },
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                wildcard: "%QUERY",            
+                url: $(obj).data("autocomplete-url") + "?query=%QUERY",
+                filter: function (autos) {
+                    // Map the remote source JSON array to a JavaScript object array
+                    return $.map(autos, function (auto) {
+                        return {
+                            value: auto.Name,
+                            id: auto.Id
+                        };
+                    });
+                }
+            },
+            limit: 1000
+        });
+
+        autos.initialize();
+
+        $(obj).typeahead({ highlight: true, minLength: 0, hint: true }, {
+            name: 'autos', displayKey: 'value', source: autos.ttAdapter()
+        }).on('typeahead:selected', function (obj, datum) {
+            onselected(obj, datum);
+        });
+
+        if ($(obj).hasClass("focus")) {
+            $(obj).focus();
+        }
+
+    };
+
+    function onselected(obj, datum) {
+        if (!obj || !obj.target || !datum) return;
+        $('#' + jQuery(obj.target).data("autocomplete-id-field")).val(datum.id.toString());
+    }
+
+    $('*[data-autocomplete-url]')
+            .each(function () {
+                autocompletewrapper($(this));
+            });
+})
+)
